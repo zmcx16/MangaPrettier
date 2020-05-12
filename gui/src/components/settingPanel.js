f7b7e92729e83fd5d34bf25fbe9854c23be53a8d
zmcx16 <zmcx16@gmail.com>
2020-05-13 01:55:59 +0800

    implement addwindow

--- gui/src/components/settingPanel.js
+++ gui/src/components/settingPanel.js
@@ -1,4 +1,5 @@
 import React, { useState, useRef, useEffect, useCallback } from 'react'
+import Popover from '@material-ui/core/Popover'
 import Button from '@material-ui/core/Button'
 import IconButton from '@material-ui/core/IconButton'
 import DeleteIcon from '@material-ui/icons/Delete'
@@ -8,6 +9,9 @@ import ListItem from '@material-ui/core/ListItem'
 import ListItemText from '@material-ui/core/ListItemText'
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction'
 import LinearProgress from '@material-ui/core/LinearProgress'
+import InputLabel from '@material-ui/core/InputLabel'
+import FormControl from '@material-ui/core/FormControl'
+import Select from '@material-ui/core/Select'
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
 
 import settingPanelStyle from "./settingPanel.module.scss"
@@ -16,14 +20,13 @@ const shortid = window.require('shortid')
 
 function SettingPanel({ settingPanelRef, filesPanelAPI, previewImagePanelAPI}) {
 
+  // effects list
   const argsList = useRef([
     { name: 'oox1', text: 'aabbbcc', value: {} },
     { name: 'oox', text: 'aabbbcc', value: {} },
     { name: 'oox', text: 'aabbbcc', value: {} },
     { name: 'oox', text: 'aabbbcc', value: {} },
   ])
-
-  const [progressBar, setProgressBar] = useState(30);
 
   const renderArgsList = ()=>{
     return argsList.current.map((value, index) => {
@@ -46,6 +49,36 @@ function SettingPanel({ settingPanelRef, filesPanelAPI, previewImagePanelAPI}) {
 
   const [argsListNodes, setArgsListNodes] = useState(renderArgsList())
 
+  // progressbar
+  const [progressBar, setProgressBar] = useState(30);
+
+  // add window
+  const [addWindow, setAddWindow] = useState(null);
+  const addWindowClick = (event) => {
+    setAddWindow(event.currentTarget)
+  }
+
+  const addWindowClose = () => {
+    setAddWindow(null);
+  }
+
+  // effects select
+  const [effectSelect, setEffectSelect] = useState({
+    name: 'effect',
+    effectType: ''
+  });
+
+  const effectSelectChange = (event) => {
+    const name = event.target.name;
+    setEffectSelect({
+      ...effectSelect,
+      [name]: event.target.value,
+    });
+  };
+
+  const openAddWindow = Boolean(addWindow)
+  const id_addWindow = openAddWindow ? 'addWindow' : undefined
+
   return (
     <div className={settingPanelStyle.settingPanel}>
       <MuiThemeProvider theme={createMuiTheme({ palette: { primary: blue, secondary: lightBlue } })}>
@@ -56,7 +89,7 @@ function SettingPanel({ settingPanelRef, filesPanelAPI, previewImagePanelAPI}) {
             </List>
           </div>
           <div className={settingPanelStyle.argsButtons}>
-            <Button variant="contained" color="primary" className={settingPanelStyle.button}>Add</Button>
+            <Button variant="contained" color="primary" className={settingPanelStyle.button} onClick={addWindowClick}>Add</Button>
             <div></div>
             <Button variant="contained" color="primary" className={settingPanelStyle.button}>Import</Button>
             <div></div>
@@ -68,6 +101,48 @@ function SettingPanel({ settingPanelRef, filesPanelAPI, previewImagePanelAPI}) {
             <div></div>
           </div>
         </div>
+        <Popover
+          id={id_addWindow}
+          open={openAddWindow}
+          anchorEl={addWindow}
+          onClose={addWindowClose}
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'left',
+          }}
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'left',
+          }}
+        >
+          <div className={settingPanelStyle.addWindow}>
+            <FormControl variant="outlined" className={settingPanelStyle.addWindowEffectSelect}>
+              <InputLabel htmlFor="effect-select">Effects</InputLabel>
+              <Select
+                native
+                value={effectSelect.effectType}
+                onChange={effectSelectChange}
+                label="Effect"
+                inputProps={{
+                  name: 'effectType',
+                  id: 'effect-select',
+                }}
+              >
+                <option value={'effect1'}>effect1</option>
+                <option value={'effect2'}>effect2</option>
+                <option value={'effect3'}>effect3</option>
+              </Select>
+            </FormControl>
+            <div className={settingPanelStyle.addWindowEffectArgs}>
+              args
+            </div>
+            <div className={settingPanelStyle.addWindowButtons}>
+              <Button variant="contained" color="primary" className={settingPanelStyle.button} onClick={() => { }}>Ok</Button>
+              <div></div>
+              <Button variant="contained" color="primary" className={settingPanelStyle.button} onClick={() => { }}>Cancel</Button>
+            </div>
+          </div>
+        </Popover>
       </MuiThemeProvider>
     </div>
   )
